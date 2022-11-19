6ecd28fe184fbf4a6e6932ff89b0b0fee83f0acc
olaDEN <97802281+olaDEN@users.noreply.github.com>
2022-11-19 12:21:27 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Ai-speech-to-speech-grammer-corrector
+A speech to speech  grammar corrector application using python and streamlit
+
+![Screenshot (106)](https://user-images.githubusercontent.com/97802281/202772404-d06c5121-a3b4-4b27-b360-aa9e41fba6e8.png)
+
+## **How to use:**
+
+1- Open the CMD panel as an administrator and write the following command:
+>> pip install -r requirements.txt.
+
+2- Clone the repository to your local.
+
+3- Open the CMD panel again, navigate to the folder where you cloned the repository and write the following command:
+>> streamlit run gr-chatbot.py
+
+4- If the browser's window did't open automatically, go to the Local URL: http://localhost:****.
+
+P.S.: **** indicates the local host shown on the CMD, e.g. 8081
+
+5- Make sure that your microphone on your local is allowing apps to access it: 
+For Windows: *Settings -> Microphone Privacy Settings -> turn "Allow apps to access your microphone on"*.
+
+6- Once the gr-chatbot.py is loaded to the browser, you can click the "Speak" button and speak clearly with short and one sentence at a time, simple sentences works better ( Ex: You is beuatiful -> You are beutiful) (Like do not read a statement and expect the chatbot to answer you :D).
+
+P.S.: You do not need to continuously pressing the speak button while you speak, just click it one and start, once you finish speaking, the chatbot will wait for couple of seconds and then will answer you (with voice and text).
+
+7- For better performance, try to refresh the site after 5 speak tries (Issue related to the pretrained model).
+
+8- Have fun ;)
+
+9- Do not forget to chack the references.  **kindly keep the source info.**
+
+**Since this is a pretrained model, the answeres may be unexpected sometimes, either because of isseus related to microphone's quality or grammar evaluation.**
---
+++ gr-chatbot.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import speech_recognition as sr
+from gtts import gTTS
+import language_tool_python  
+from gtts import gTTS
+from io import BytesIO
+import pygame
+import time 
+
+# # Source: https://github.com/olaDEN
+
+colT1,colT2 = st.columns([1,3])
+with colT2:
+    st.title("Ai-GrammerBot")
+
+
+st.write("Hey there \U0001F44B \U0001F603 please click the \"Speak\" button and say something, i will check your grammar \U0001F609. ")
+
+recognizer = sr.Recognizer()
+recognizer.energy_threshold = 300
+mic = sr.Microphone()
+left, center, center2, right, right2 = st.columns(5)
+
+isspeak = center2.button("Speak")
+
+with mic as source:
+    if isspeak:  
+        audio = recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        recognizer.pause_threshold = 1
+        audio = recognizer.listen(source, timeout=3, phrase_time_limit=8)
+        try:
+            # take user input
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+            
+            def speak(text, language='en'):
+                mp3_fo = BytesIO()
+                tts = gTTS(text, lang=language)
+                tts.write_to_fp(mp3_fo)
+                pygame.mixer.music.load(mp3_fo, 'mp3')
+                time.sleep(0.1)
+                pygame.mixer.music.play()
+                # return mp3_fo
+                
+            # creating the tool  
+            my_tool = language_tool_python.LanguageTool('en-US', config={ 'cacheSize': 1000, 'pipelineCaching': True })  
+            # given text  
+            my_text = my_tool.correct(text)
+            # correction 
+            if len(my_text)==len(text):
+              st.markdown(f"## GrammerBot reply:")
+              correct_text = "You said \"" + text + "\" and it is correct."
+              pygame.init()
+              pygame.mixer.init()
+              # sound.seek(0)
+              output = speak(correct_text)
+              st.success(f"{correct_text}") 
+            else:
+              # <h1 style='text-align: center; color: red;'>GrammerBot reply:</h1>
+              st.markdown("GrammerBot reply:", unsafe_allow_html=True)
+              correct_text = "The correct way to say that: \"" + my_text + "\""
+              pygame.init()
+              pygame.mixer.init()
+              # sound.seek(0)
+              output = speak(correct_text)
+              st.success(f"{correct_text}") 
+              st.error(f"What you said: {text}")
+
+        except sr.UnknownValueError:
+            st.write("Speak again please")
+        except sr.RequestError:
+            st.write("Speech Service down")
+
+# # Source: https://github.com/olaDEN
+
